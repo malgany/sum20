c1f45296a98e3af0efffc697ceebeb5f33e22e04
tatisiqueira <tatianess.siqueira@gmail.com>
2022-05-29 16:16:42 -0400

    first commit!

---
+++ js/All.js
@@ -0,0 +1,177 @@
+function All() {
+
+    this.init = init;
+
+    const keys = [
+        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+        ['11', '12', '13', '14', '15', '16', '19', '18', '19', '20'],
+        ['0', '⏎', '←']
+    ];
+
+    const g = document.getElementById("game");
+    const k = document.getElementById("keyboard");
+
+    let gbox = null;
+    let kbox = null;
+
+    const target = 20;
+    const numbers = generateNumbers(target);
+
+    let line = 1;
+
+    function init() {
+
+        createGameSquad();
+        gbox = g.getElementsByClassName("box");
+        kbox = k.getElementsByClassName("box");
+        gbox[0].classList.add("active");
+        createGameKeyboard();
+        addClickEvents();
+        document.getElementById("w" + line).style['pointer-events'] = 'auto';
+    }
+
+    function createGameSquad() {
+        for (let i = 1; i < 7; i++) {
+            var box = '<div id="w' + i + '" class="word">\n';
+            for (let j = 1; j < 6; j++)
+                box += '<div class="box" data-position="' + j + '"></div>\n';
+            box += '</div>';
+            g.innerHTML += box;
+        }
+    }
+
+    function createGameKeyboard() {
+        keys.forEach((N1) => {
+            k.innerHTML += '<div  class="teclado">\n';
+            N1.forEach((N2) => {
+                if (N2 === '⏎' || N2 === '←') {
+                    k.innerHTML += '<div class="box white" data-codigo="' + N2 + '">' + N2 + '</div>\n';
+                } else {
+                    k.innerHTML += '<div class="box gray" data-codigo="' + N2 + '">' + N2 + '</div>\n';
+                }
+            });
+            k.innerHTML += '</div>\n';
+        });
+    }
+
+    function addClickEvents() {
+
+        for (i = 0; i < gbox.length; i++) {
+            gbox[i].addEventListener('click', selecionaBox, false);
+        }
+
+        for (i = 0; i < kbox.length; i++) {
+            kbox[i].addEventListener('click', teclaKeyboard, false);
+        }
+
+        document.addEventListener('keypress', teclaKeyboard, false);
+    }
+
+    function selecionaBox() {
+        for (let i = 0; i < gbox.length; i++) {
+            gbox[i].classList.remove("active");
+        }
+        this.classList.add("active");
+    }
+
+    function teclaKeyboard() {
+
+        let index = g.getElementsByClassName("active")[0];
+        const codigo = (event.type == "keypress") ? event.key.toUpperCase() : this.getAttribute("data-codigo");
+
+        if (codigo.length > 0 && codigo >= 0 && codigo <= 20 && !(typeof index === 'undefined')) {
+
+            index.innerText = codigo;
+            index.className = "box gray";
+
+            // Pula para o próximo elemento
+            var nS = index.nextElementSibling;
+            if (nS != null) {
+                nS.classList.add("active");
+            } else {
+                $('#w' + line + ' div:eq(4)').addClass('active');
+            }
+
+        } else if (codigo === '⏎') {
+            validateLine();
+        } else if (codigo === '←' && index !== undefined && index !== null) {
+            if (index.innerText !== '') {
+                index.innerText = '';
+                index.className = "box white active";
+            } else {
+                index.innerText = '';
+                index.className = "box white";
+
+                let nS = index.previousElementSibling;
+                if (nS != null) {
+                    nS.innerText = '';
+                    nS.className = "box white";
+                    nS.classList.add("active");
+                } else {
+                    $('#w' + line + ' div:eq(0)').addClass('active');
+                }
+            }
+        }
+    }
+
+    function validateLine() {
+        let sum = 0;
+        let add_val = [0, 5, 10, 15, 20, 25];
+        let numbers_local = [numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]];
+        for (let v = 0; v < 5; v++) {
+            sum += parseInt(gbox[add_val[line - 1] + v].innerText);
+        }
+
+        if (sum !== target) {
+            $(`#w${line}`).addClass("treme-active");
+            setTimeout(() => {
+                $(`#w${line}`).removeClass("treme-active");
+            }, 500);
+            return;
+        }
+
+        let win = 0;
+        for (let w = 0; w < 5; w++) {
+            let val = parseInt(gbox[add_val[line - 1] + w].innerText);
+
+            if (val === numbers_local[w]) {
+                gbox[add_val[line - 1] + w].className = "box green";
+                $(`[data-codigo=${val}]`).removeClass("gray");
+                $(`[data-codigo=${val}]`).addClass("green");
+                numbers_local[w] = -1;
+                win++;
+            }
+        }
+
+        if (win === 5) {
+            setTimeout(() => {
+                $('.modal').show();
+            }, 500);
+            return;
+        }
+
+        for (let w = 0; w < 5; w++) {
+            let val = parseInt(gbox[add_val[line - 1] + w].innerText);
+
+            if (numbers_local[w] === -1) {
+                continue;
+            }
+            $(`[data-codigo=${val}]`).removeClass("gray");
+
+            if (numbers_local.includes(val)) {
+                gbox[add_val[line - 1] + w].className = "box orange";
+                $(`[data-codigo=${val}]`).addClass("orange");
+            } else {
+                $(`[data-codigo=${val}]`).addClass("delta");
+            }
+        }
+
+        document.getElementById("w" + line).style['pointer-events'] = 'none';
+        line++;
+        document.getElementById("w" + line).style['pointer-events'] = 'auto';
+
+        let index = g.getElementsByClassName("active")[0];
+        $(index).removeClass("active");
+        $('#w' + line + ' div:eq(0)').addClass('active');
+    }
+}
---
+++ js/functions.js
@@ -0,0 +1,65 @@
+function generateNumbers(target) {
+
+    let n1 = getRandomArbitrary(0, target);
+    let n2 = getRandomArbitrary(0, (target - n1));
+
+    const lines = [
+        {min: n1, max: n1},
+        {min: n2, max: n2},
+        {min: 0, max: target},
+        {min: 0, max: target},
+        {min: 0, max: target}];
+
+    const result = [];
+
+    for (let x = 0; x < lines.length; x++) {
+        result[x] = lines[x].min;
+        target -= lines[x].min;
+    }
+
+    function createRoulette() {
+        const ret = [];
+        let sum = 0;
+        for (let i = 0; i < lines.length; i++) {
+            const valor = lines[i].max - result[i];
+            ret.push(valor + sum);
+            sum += valor;
+        }
+        return [ret, sum];
+    }
+
+    for (let y = 0; y < target; y++) {
+        const roulette = createRoulette();
+        const draw = Math.floor(Math.random() * roulette[1]);
+        for (let z = 0; z < lines.length; z++)
+            if (draw < roulette[0][z]) {
+                result[z]++;
+                break;
+            }
+    }
+
+    return shuffleArray(result);
+}
+
+function generateNumbersV2(target) {
+    let n1 = getRandomArbitrary(0, target);
+    let n2 = getRandomArbitrary(0, (target - n1));
+    let n3 = getRandomArbitrary(0, (target - n1 - n2));
+    let n4 = getRandomArbitrary(0, (target - n1 - n2 - n3));
+    let n5 = target - n1 - n2 - n3 - n4;
+
+    return shuffleArray([n1, n2, n3, n4, n5]);
+}
+
+function getRandomArbitrary(min, max) {
+    return parseInt(Math.random() * (max - min) + min);
+}
+
+function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    console.log(arr);
+    return arr;
+}
